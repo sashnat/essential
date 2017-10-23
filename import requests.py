76e639ee15fb4d7c2efe518105dd55b2bde3aaca
Nataliya Sashnikova <sashnat@yahoo.com>
2017-10-23 20:51:21 +0300

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,5 +1,5 @@
 import requests
-url = 'https://stepic.org/media/attachments/course67/3.6.2/626.txt'
+url = 'https://sjfjrnkjrnrkngkng.org/media/attachments/course67/3.6.2/626.txt'
 r = requests.get(url.strip())
 print(len(r.text.splitlines()))
 # or print(r.text.count('\n'))
