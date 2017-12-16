081910a6b03378eccfdd7a370a9a8b719f4027e0
Nataliya Sashnikova <sashnat@yahoo.com>
2017-12-16 22:11:54 +0200

    list of all files

---
+++ all files in folders.py
@@ -0,0 +1,24 @@
+import os
+tree = os.walk('Z:\Python')
+print(tree)
+
+pdir = 'Z:\Python'
+contdir = []
+for i in os.walk(pdir):
+    contdir.append(i)
+
+for i in contdir:
+    print (i)
+
+for d, dirs, files in os.walk('Z:\Python'):
+    for f in files:
+        print (f)
+'''Для получения полных адресов подойдет функция 
+os.path.join(). С ее помощью можно объединить первый элемент кортежа, содержащий полный 
+путь, с именем каждого файла:'''
+path_f = []
+for d, dirs, files in os.walk('Z:\Python'):
+    for f in files:
+        path = os.path.join(d,f) # формирование адреса
+        path_f.append(path) # добавление адреса в список
+print(path_f )
