34bd5025d07544141965e324f237406b6aa16598
Nataliya Sashnikova <sashnat@yahoo.com>
2018-08-17 16:22:28 +0300

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,26 @@
+# https://habr.com/post/141411/
+
+def makebold(fn):
+    def wrapped():
+        return "<b>" + fn() + "</b>"
+    return wrapped
+
+def makeitalic(fn):
+    def wrapped():
+        return "<i>" + fn() + "</i>"
+    return wrapped
+
+@makebold
+@makeitalic
+def hello():
+    return "hello habr"
+
+print (hello()) ## выведет <b><i>hello habr</i></b>
+
+# --------без декоратора-------------------
+
+def hello1():
+    return "hello habr"
+
+wer = makebold(hello1)
+print (wer())          # выведет <b>hello habr</b>
